bf67f5d85055f4c160c6391ff9f13a645089a1b3
ALL-MIGHTY-LABT\karel <mirakl834@gmail.com>
2017-11-23 19:15:12 -0500

    update fixes to the joins

--- DataLoader.sql
+++ DataLoader.sql
@@ -1,88 +1,122 @@
-BULK INSERT Title
-FROM 'C:\CSVData\MOCK_tblTitle.csv'
+BULK INSERT Item.Title
+FROM 'C:\library_db\WestMunicipalLibrary\MOCK_tblTitle.csv'
 WITH
   (
     FIRSTROW = 2,
     FIELDTERMINATOR = ',',  --CSV field delimiter
-    ROWTERMINATOR = '\n',   --Use to shift the control to next row
-    TABLOCK
+    ROWTERMINATOR = '\n'   --Use to shift the control to next row
   )
 ;
 go
-BULK INSERT Item
-FROM 'C:\CSVData\MOCK_tblItem.csv'
+
+
+select * from Item.Title
+;
+go
+
+
+BULK INSERT Item.Item
+FROM 'C:\library_db\WestMunicipalLibrary\MOCK_tblItem.csv'
 WITH
   (
     FIRSTROW = 2,
     FIELDTERMINATOR = ',',  --CSV field delimiter
-    ROWTERMINATOR = '\n',   --Use to shift the control to next row
-    TABLOCK
+    ROWTERMINATOR = '\n'  --Use to shift the control to next row
   )
 ;
 go
-BULK INSERT Copy
-FROM 'C:\CSVData\MOCK_tblCopy.csv'
+
+select * from Item.Item
+;
+go
+
+BULK INSERT Item.Copy
+FROM 'C:\library_db\WestMunicipalLibrary\MOCK_tblCopy.csv'
 WITH
   (
     FIRSTROW = 2,
     FIELDTERMINATOR = ',',  --CSV field delimiter
-    ROWTERMINATOR = '\n',   --Use to shift the control to next row
-    TABLOCK
+    ROWTERMINATOR = '\n'   --Use to shift the control to next row
   )
 ;
 go
-BULK INSERT Member
-FROM 'C:\CSVData\MOCK_tblMember.csv'
+
+select * from Item.Copy
+;
+go
+
+BULK INSERT Member.Member
+FROM 'C:\library_db\WestMunicipalLibrary\MOCK_tblMember.csv'
 WITH
   (
     FIRSTROW = 2,
     FIELDTERMINATOR = ',',  --CSV field delimiter
-    ROWTERMINATOR = '\n',   --Use to shift the control to next row
-    TABLOCK
+    ROWTERMINATOR = '\n'   --Use to shift the control to next row
   )
 ;
 go
-BULK INSERT Adult
-FROM 'C:\CSVData\MOCK_tblAdult.csv'
+
+select * from Member.Member
+;
+go
+
+BULK INSERT Member.Adult
+FROM 'C:\library_db\WestMunicipalLibrary\MOCK_tblAdult.csv'
 WITH
   (
     FIRSTROW = 2,
     FIELDTERMINATOR = ',',  --CSV field delimiter
-    ROWTERMINATOR = '\n',   --Use to shift the control to next row
-    TABLOCK
+    ROWTERMINATOR = '\n'   --Use to shift the control to next row
   )
 ;
 go
-BULK INSERT Juvenile
-FROM 'C:\CSVData\MOCK_tblJuvenile.csv'
+
+select * from Member.Adult
+;
+go
+
+
+BULK INSERT Member.Juvenile
+FROM 'C:\library_db\WestMunicipalLibrary\MOCK_tblJuvenile.csv'
 WITH
   (
     FIRSTROW = 2,
     FIELDTERMINATOR = ',',  --CSV field delimiter
-    ROWTERMINATOR = '\n',   --Use to shift the control to next row
-    TABLOCK
+    ROWTERMINATOR = '\n'   --Use to shift the control to next row
   )
 ;
 go
-BULK INSERT Reservation
-FROM 'C:\CSVData\MOCK_tblReservation.csv'
+
+select * from Member.Juvenile
+;
+go
+
+BULK INSERT Activity.Reservation
+FROM 'C:\library_db\WestMunicipalLibrary\MOCK_tblReservation.csv'
 WITH
   (
     FIRSTROW = 2,
     FIELDTERMINATOR = ',',  --CSV field delimiter
-    ROWTERMINATOR = '\n',   --Use to shift the control to next row
-    TABLOCK
+    ROWTERMINATOR = '\n'   --Use to shift the control to next row
   )
 ;
 go
-BULK INSERT Loan
-FROM 'C:\CSVData\MOCK_tblLoan.csv'
+
+select * from Activity.Reservation
+;
+go
+
+BULK INSERT Activity.Loan
+FROM 'C:\library_db\WestMunicipalLibrary\MOCK_tblLoan.csv'
 WITH
   (
     FIRSTROW = 2,
     FIELDTERMINATOR = ',',  --CSV field delimiter
-    ROWTERMINATOR = '\n',   --Use to shift the control to next row
-    TABLOCK
+    ROWTERMINATOR = '\n'   --Use to shift the control to next row
   )
+;
+go
+
+select * from Activity.Loan
 ;
 go
